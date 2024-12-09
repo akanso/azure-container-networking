5a5bc1f746f1ef357f671469d02512dec2691338
QxBytes <alexanderlew@microsoft.com>
2024-12-09 13:13:03 -0800

    draft run netperf script stage

---
+++ hack/scripts/k8s-netperf.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+resources=(
+  "deployment/netperf-pod"
+  "ds/netperf-pod"
+  "ds/netperf-host"
+)
+
+sudo PERL_MM_USE_DEFAULT=1 perl -MCPAN -e 'install JSON::Parse'
+
+for resource in "${resources[@]}"; do
+  while [[ $(kubectl get "$resource" -o jsonpath='{.status.readyReplicas}') != $(kubectl get "$resource" -o jsonpath='{.spec.replicas}') ]]; do
+    echo "Waiting for $resource to be ready..."
+    sleep 1
+  done
+done
+
+echo "All resources are ready!"
+
+perl runNetPerfTest.pl > out.log
+
+echo "Finished Perf Test"
+
+kubectl delete deployment netperf-pod
+kubectl delete ds netperf-pod
+kubectl delete ds netperf-host
+kubectl delete svc netperf-server
