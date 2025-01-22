35a33ae16a054db2f76f42ca7d056f97a30ba484
Paul Yu <paulyu01@outlook.com>
2025-01-21 21:10:36 -0500

    fix a bug

--- cni/network/invoker_cns.go
+++ cni/network/invoker_cns.go
@@ -362,7 +362,7 @@ func getRoutes(cnsRoutes []cns.Route, skipDefaultRoutes bool) ([]network.RouteIn
 		}
 
 		gw := net.ParseIP(route.GatewayIPAddress)
-		if gw == nil && skipDefaultRoutes {
+		if skipDefaultRoutes {
 			return nil, errors.Wrap(errInvalidGatewayIP, route.GatewayIPAddress)
 		}
 
