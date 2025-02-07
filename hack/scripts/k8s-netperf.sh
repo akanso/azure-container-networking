9a52208455ca92ec4fb14186df906a1bac397dcd
QxBytes <alexanderlew@microsoft.com>
2025-02-06 16:47:50 -0800

    replace throughput test with latency test netperf

--- hack/scripts/k8s-netperf.sh
+++ hack/scripts/k8s-netperf.sh
@@ -17,11 +17,10 @@ done
 echo "All resources are ready!"
 
 for i in {1..1}; do
-    perl runNetPerfTestIperfOnly.pl --nobaseline > "out-${i}.log"
+    perl runLatencyOnly.pl --nobaseline > "out-${i}.log"
     kubectl get pods -owide | grep netperf > "out-netperf-pods-${i}.log"
-    cat "out-${i}.log" | grep "Localhost Iperf throughput test between POD" > "out-local-${i}.log"
-    cat "out-${i}.log" | grep "InterNode Iperf throughput test between POD" > "out-inter-${i}.log"
-    grep -oP "(?<=: )\d+\.\d+(?= Gbits/sec )" "out-${i}.log" > "out-raw-gbs-${i}.log"
+    cat "out-${i}.log" | grep "Localhost NetPerf TCP_RR test between POD" > "out-local-${i}.log"
+    cat "out-local-${i}.log" | grep -o 'transaction rate): [0-9]\+' | sed 's/[^0-9]//g' > "out-local-${i}-raw.log"
     echo "Finished Perf Test Iter ${i}"
 done
 
