2ddc5174d65e3cf8aa6ef27337baaf10ff92b9c8
krunaljain <krunal.bitsy@gmail.com>
2025-03-01 21:54:46 -0800

    Update script

--- hack/scripts/cil-script.sh
+++ hack/scripts/cil-script.sh
@@ -11,30 +11,32 @@ install=helm
 echo "sufixes ${sufixes}"
 cd ../..
 for unique in $sufixes; do
+    if (( unique % 2 == 0 )); then
+        cluster_id=2  # Even unique, set cluster_id to 2
+    else
+        cluster_id=1 
+    fi 
     make -C ./hack/aks $clusterType \
         AZCLI=az REGION=westus2 SUB=$SUB \
         CLUSTER=${clusterPrefix}-${unique} \
         POD_CIDR=192.${unique}0.0.0/16 SVC_CIDR=192.${unique}1.0.0/16 DNS_IP=192.${unique}1.0.10 \
         VNET_PREFIX=10.${unique}0.0.0/16 SUBNET_PREFIX=10.${unique}0.0.0/16
-        echo "Cluster Prefix: ${clusterPrefix}"
-        echo "Unique ID: ${unique}"
-        echo "Resource Group: ${clusterPrefix}-${unique}-rg"
-        echo "Cluster Name: ${clusterPrefix}-${unique}"
-        cilium install -n kube-system cilium cilium/cilium --version v1.16.1 \
-                --set azure.resourceGroup=${clusterPrefix}-${unique}-rg --set cluster.id=${unique} \
+    kubectl config use-context ${clusterPrefix}-${unique}
+    helm upgrade --install -n kube-system cilium cilium/cilium --version v1.16.1 \
+                --set azure.resourceGroup=${clusterPrefix}-${unique}-rg \
+                --set cluster.id=$cluster_id \
                 --set ipam.operator.clusterPoolIPv4PodCIDRList='{192.'${unique}'0.0.0/16}' \
-                --set hubble.enabled=false \
                 --set cluster.name=${clusterPrefix}-${unique} \
                 --set debug.enabled=true \
                 --set debug.verbose="datapath" \
                 --set endpointRoutes.enabled=true \
                 --set bpf.hostLegacyRouting=true \
                 --set envoy.enabled=false
+    cd ../cilium
+    ./deploy_image.sh krunaljainhybridtest ${clusterPrefix}-${unique}
+    cd ../azure-container-networking
 done
-cd ../cilium
-./deploy_image.sh krunaljaincustom ${clusterPrefix}-${sufix1}
-./deploy_image.sh krunaljaincustom ${clusterPrefix}-${sufix2}
-cd ../azure-container-networking/hack/scripts
+
 VNET_ID1=$(az network vnet show \
     --resource-group "${clusterPrefix}-${sufix1}-rg" \
     --name "${clusterPrefix}-${sufix1}-vnet" \
@@ -60,12 +62,8 @@ az network vnet peering create \
     --allow-vnet-access
 
 
-cilium clustermesh enable --context ${clusterPrefix}-${sufix1} --enable-kvstoremesh=true
-cilium clustermesh enable --context ${clusterPrefix}-${sufix2} --enable-kvstoremesh=true
-
-
-cilium clustermesh status --context ${clusterPrefix}-${sufix1} --wait
-cilium clustermesh status --context ${clusterPrefix}-${sufix2} --wait
+cilium clustermesh enable --context ${clusterPrefix}-${sufix1} --service-type NodePort --enable-kvstoremesh=true
+cilium clustermesh enable --context ${clusterPrefix}-${sufix2} --service-type NodePort --enable-kvstoremesh=true
 
 # # CA is passed between clusters in this step
 cilium clustermesh connect --context ${clusterPrefix}-${sufix1} --destination-context ${clusterPrefix}-${sufix2}
