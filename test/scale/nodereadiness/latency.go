ee61ca3fc57ab956b242ccad57175168181513ae
jshr-w <shjayaraman@microsoft.com>
2025-03-05 14:27:50 -0800

    create nnccontroller

--- test/scale/nodereadiness/latency.go
+++ test/scale/nodereadiness/latency.go
@@ -5,8 +5,6 @@ import (
 	"flag"
 	"fmt"
 	"net/http"
-	"os"
-	"os/signal"
 	"path/filepath"
 	"sync"
 	"time"
@@ -17,6 +15,8 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime/schema"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	"k8s.io/apimachinery/pkg/util/wait"
 	"k8s.io/apimachinery/pkg/watch"
 	"k8s.io/client-go/dynamic"
 	"k8s.io/client-go/dynamic/dynamicinformer"
@@ -24,6 +24,7 @@ import (
 	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/util/homedir"
+	"k8s.io/client-go/util/workqueue"
 )
 
 var (
@@ -49,35 +50,139 @@ var (
 		Name: "node_ready",
 		Help: "Number of nodes that are ready",
 	})
-
-	nodeCreation = make(map[string]time.Time)
-	nncCreation  = make(map[string]time.Time)
-	nncReady     = make(map[string]time.Time)
 )
 
 // WIP
-// Controller is the controller implementation for Foo resources
-// type Controller struct {
-// 	// kubeclientset is a standard kubernetes clientset
-// 	kubeclientset kubernetes.Interface
-// 	// sampleclientset is a clientset for our own API group
-// 	sampleclientset clientset.Interface
+type NNCController struct {
+	dynamicClient dynamic.Interface
+	workqueue     workqueue.TypedRateLimitingInterface[interface{}]
 
-// 	deploymentsLister appslisters.DeploymentLister
-// 	deploymentsSynced cache.InformerSynced
-// 	foosLister        listers.FooLister
-// 	foosSynced        cache.InformerSynced
+	clientset   *kubernetes.Clientset
+	nodeWatcher watch.Interface
 
-// 	// workqueue is a rate limited work queue. This is used to queue work to be
-// 	// processed instead of performing it as soon as a change happens. This
-// 	// means we can ensure we only process a fixed amount of resources at a
-// 	// time, and makes it easy to ensure we are never processing the same item
-// 	// simultaneously in two different workers.
-// 	workqueue workqueue.TypedRateLimitingInterface[cache.ObjectName]
-// 	// recorder is an event recorder for recording Event resources to the
-// 	// Kubernetes API.
-// 	recorder record.EventRecorder
-// }
+	nodeCreation map[string]time.Time
+	nncCreation  map[string]time.Time
+	nncReady     map[string]time.Time
+
+	sync.RWMutex
+}
+
+func NewNNCController(
+	ctx context.Context,
+	dynamicClient dynamic.Interface,
+	clientset *kubernetes.Clientset) *NNCController {
+	// TODO
+	workqueue := workqueue.NewTypedRateLimitingQueue(
+		workqueue.NewTypedItemExponentialFailureRateLimiter[interface{}](5*time.Millisecond, 1000*time.Second),
+	)
+
+	controller := &NNCController{
+		dynamicClient: dynamicClient,
+		workqueue:     workqueue,
+	}
+
+	factory := dynamicinformer.NewFilteredDynamicSharedInformerFactory(dynamicClient, time.Minute, corev1.NamespaceAll, nil)
+	informer := factory.ForResource(nnc).Informer()
+
+	informer.AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc:    controller.addNNC,
+		UpdateFunc: controller.updateNNC,
+		DeleteFunc: func(obj interface{}) {},
+	})
+
+	nodeWatcher, err := clientset.CoreV1().Nodes().Watch(context.Background(), metav1.ListOptions{})
+	if err != nil {
+		panic(err.Error())
+	}
+
+	go func() {
+		for event := range nodeWatcher.ResultChan() {
+			switch event.Type {
+			case watch.Added:
+				name := event.Object.(*corev1.Node).Name
+				timestamp := event.Object.(*corev1.Node).CreationTimestamp.Time
+				if _, ok := controller.nodeCreation[name]; !ok {
+					controller.nodeCreation[name] = timestamp
+					fmt.Printf("Node added: %v at %v \n", name, timestamp)
+				}
+			case watch.Modified:
+			case watch.Deleted:
+			case watch.Error:
+			case watch.Bookmark:
+			}
+		}
+	}()
+
+	return controller
+}
+
+func (c *NNCController) Run(ctx context.Context, workers int) {
+	defer utilruntime.HandleCrash()
+	defer c.workqueue.ShutDown()
+
+	for i := 0; i < workers; i++ {
+		go wait.UntilWithContext(ctx, c.runWorker, time.Second)
+	}
+
+	<-ctx.Done()
+}
+
+func (c *NNCController) runWorker(ctx context.Context) {
+	for c.processNextWorkItem(ctx) {
+	}
+}
+
+func (c *NNCController) processNextWorkItem(ctx context.Context) bool {
+	obj, shutdown := c.workqueue.Get()
+
+	if shutdown {
+		return false
+	}
+
+	err := func(obj interface{}) error {
+		defer c.workqueue.Done(obj)
+
+		c.workqueue.AddRateLimited(obj)
+		c.workqueue.Forget(obj)
+
+		return nil
+	}(obj)
+	if err != nil {
+		utilruntime.HandleError(err)
+		return true
+	}
+
+	return true
+}
+
+func (c *NNCController) addNNC(obj interface{}) {
+	name := obj.(*unstructured.Unstructured).GetName()
+	timestamp := obj.(*unstructured.Unstructured).GetCreationTimestamp().Time
+	if _, ok := c.nncCreation[name]; !ok {
+		c.nncCreation[name] = timestamp
+		fmt.Printf("NNC created: %v at %v \n", name, timestamp)
+		if _, ok := c.nodeCreation[name]; ok {
+			latency := c.nncCreation[name].Sub(c.nodeCreation[name])
+			nncLatency.WithLabelValues("nodetonnc").Observe(latency.Seconds())
+		}
+	}
+}
+
+func (c *NNCController) updateNNC(oldObj, newObj interface{}) {
+	if newObj.(*unstructured.Unstructured).Object != nil && newObj.(*unstructured.Unstructured).Object["status"] != nil {
+		timestamp := time.Now() // probs not super accurate
+		name := newObj.(*unstructured.Unstructured).GetName()
+		if _, ok := c.nncReady[name]; !ok {
+			c.nncReady[name] = timestamp
+			fmt.Printf("NNC ready: %v at %v \n", name, timestamp)
+			if _, ok := c.nncCreation[name]; ok {
+				latency := c.nncReady[name].Sub(c.nncCreation[name])
+				nncLatency.WithLabelValues("nncready").Observe(latency.Seconds())
+				nncReadyCount.Inc()
+			}
+		}
+	}
+}
 
 func main() {
 	// todo: Allow user to pass kubeconfig arg.
@@ -105,84 +210,80 @@ func main() {
 	}
 
 	prometheus.MustRegister(nncLatency)
-	//ctx := context.Background()
 
-	wg := sync.WaitGroup{} // todo
-	wg.Add(2)
-	go watchNodes(clientset, &wg)
-	go watchNNC(dynamicClient, &wg)
+	ctx := context.Background()
+	nncController := NewNNCController(ctx, dynamicClient, clientset)
+	go nncController.Run(ctx, 20)
 
 	http.Handle("/metrics", promhttp.Handler())
 	http.ListenAndServe(":2112", nil)
-
-	wg.Wait()
 }
 
-func watchNodes(clientset *kubernetes.Clientset, wg *sync.WaitGroup) {
-	defer wg.Done()
+// func watchNodes(clientset *kubernetes.Clientset, wg *sync.WaitGroup) {
+// 	defer wg.Done()
 
-	nodeWatcher, err := clientset.CoreV1().Nodes().Watch(context.Background(), metav1.ListOptions{})
-	if err != nil {
-		panic(err.Error())
-	}
+// 	nodeWatcher, err := clientset.CoreV1().Nodes().Watch(context.Background(), metav1.ListOptions{})
+// 	if err != nil {
+// 		panic(err.Error())
+// 	}
 
-	for event := range nodeWatcher.ResultChan() {
-		switch event.Type {
-		case watch.Added:
-			name := event.Object.(*corev1.Node).Name
-			timestamp := event.Object.(*corev1.Node).CreationTimestamp.Time
-			if _, ok := nodeCreation[name]; !ok {
-				nodeCreation[name] = timestamp
-				fmt.Printf("Node added: %v at %v \n", name, timestamp)
-			}
-		case watch.Modified:
-		case watch.Deleted:
-		case watch.Error:
-		case watch.Bookmark:
-		}
-	}
-}
+// 	for event := range nodeWatcher.ResultChan() {
+// 		switch event.Type {
+// 		case watch.Added:
+// 			name := event.Object.(*corev1.Node).Name
+// 			timestamp := event.Object.(*corev1.Node).CreationTimestamp.Time
+// 			if _, ok := nodeCreation[name]; !ok {
+// 				nodeCreation[name] = timestamp
+// 				fmt.Printf("Node added: %v at %v \n", name, timestamp)
+// 			}
+// 		case watch.Modified:
+// 		case watch.Deleted:
+// 		case watch.Error:
+// 		case watch.Bookmark:
+// 		}
+// 	}
+// }
 
-func watchNNC(dynamicClient *dynamic.DynamicClient, wg *sync.WaitGroup) {
-	defer wg.Done()
+// func watchNNC(dynamicClient *dynamic.DynamicClient, wg *sync.WaitGroup) {
+// 	defer wg.Done()
 
-	// TODO: Should we skip cache syncing on start up? for now, initial node startup latencies are also counted
-	factory := dynamicinformer.NewFilteredDynamicSharedInformerFactory(dynamicClient, time.Minute, corev1.NamespaceAll, nil)
-	informer := factory.ForResource(nnc).Informer()
+// 	// TODO: Should we skip cache syncing on start up? for now, initial node startup latencies are also counted
+// 	factory := dynamicinformer.NewFilteredDynamicSharedInformerFactory(dynamicClient, time.Minute, corev1.NamespaceAll, nil)
+// 	informer := factory.ForResource(nnc).Informer()
 
-	informer.AddEventHandler(cache.ResourceEventHandlerFuncs{
-		AddFunc: func(obj interface{}) {
-			name := obj.(*unstructured.Unstructured).GetName()
-			timestamp := obj.(*unstructured.Unstructured).GetCreationTimestamp().Time
-			if _, ok := nncCreation[name]; !ok {
-				nncCreation[name] = timestamp
-				fmt.Printf("NNC created: %v at %v \n", name, timestamp)
-				if _, ok := nodeCreation[name]; ok {
-					latency := nncCreation[name].Sub(nodeCreation[name])
-					nncLatency.WithLabelValues("nodetonnc").Observe(latency.Seconds())
-				}
-			}
-		},
-		UpdateFunc: func(oldObj, newObj interface{}) {
-			if newObj.(*unstructured.Unstructured).Object != nil && newObj.(*unstructured.Unstructured).Object["status"] != nil {
-				timestamp := time.Now() // probs not super accurate
-				//nncStatus := newObj.(*unstructured.Unstructured).Object["status"]
-				name := newObj.(*unstructured.Unstructured).GetName()
-				if _, ok := nncReady[name]; !ok {
-					nncReady[name] = timestamp
-					fmt.Printf("NNC ready: %v at %v \n", name, timestamp)
-					if _, ok := nncCreation[name]; ok {
-						latency := nncReady[name].Sub(nncCreation[name])
-						nncLatency.WithLabelValues("nncready").Observe(latency.Seconds())
-						nncReadyCount.Inc()
-					}
-				}
-			}
-		},
-		DeleteFunc: func(obj interface{}) {},
-	})
-	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
-	defer cancel()
+// 	informer.AddEventHandler(cache.ResourceEventHandlerFuncs{
+// 		AddFunc: func(obj interface{}) {
+// 			name := obj.(*unstructured.Unstructured).GetName()
+// 			timestamp := obj.(*unstructured.Unstructured).GetCreationTimestamp().Time
+// 			if _, ok := nncCreation[name]; !ok {
+// 				nncCreation[name] = timestamp
+// 				fmt.Printf("NNC created: %v at %v \n", name, timestamp)
+// 				if _, ok := nodeCreation[name]; ok {
+// 					latency := nncCreation[name].Sub(nodeCreation[name])
+// 					nncLatency.WithLabelValues("nodetonnc").Observe(latency.Seconds())
+// 				}
+// 			}
+// 		},
+// 		UpdateFunc: func(oldObj, newObj interface{}) {
+// 			if newObj.(*unstructured.Unstructured).Object != nil && newObj.(*unstructured.Unstructured).Object["status"] != nil {
+// 				timestamp := time.Now() // probs not super accurate
+// 				//nncStatus := newObj.(*unstructured.Unstructured).Object["status"]
+// 				name := newObj.(*unstructured.Unstructured).GetName()
+// 				if _, ok := nncReady[name]; !ok {
+// 					nncReady[name] = timestamp
+// 					fmt.Printf("NNC ready: %v at %v \n", name, timestamp)
+// 					if _, ok := nncCreation[name]; ok {
+// 						latency := nncReady[name].Sub(nncCreation[name])
+// 						nncLatency.WithLabelValues("nncready").Observe(latency.Seconds())
+// 						nncReadyCount.Inc()
+// 					}
+// 				}
+// 			}
+// 		},
+// 		DeleteFunc: func(obj interface{}) {},
+// 	})
+// 	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+// 	defer cancel()
 
-	informer.Run(ctx.Done())
-}
+// 	informer.Run(ctx.Done())
+// }
